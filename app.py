0d40d93c819213395ac878ad2aef57761773ceee
louisconway <50680487+louisconway@users.noreply.github.com>
2020-06-24 08:26:26 +1200

    Using loops I have created a basic RSI algo, now to add real data and make some fake money
    Signed-off-by: louisconway <50680487+louisconway@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,62 +1,93 @@
-import alpaca_trade_api as tradeapi 
-from alpha_vantage.timeseries import TimeSeries 
-from alpha_vantage.techindicators import TechIndicators
+import alpaca_trade_api as tradeapi
 import json
 import datetime
 import pandas as pd 
-
-
-# Alpaca Markets API Connection
-API_KEY = "PK6VPNP2RHFM0XSYHB13"
-SECRET_KEY = "m1kvvD11jhqsFWVYyFp2d9tmvoZumMpIPZKBtCCg"
-BASE_URL = "https://paper-api.alpaca.markets"
-api = tradeapi.REST (
-    base_url =BASE_URL,
-    key_id=API_KEY,
-    secret_key=SECRET_KEY
-)
-
-# Alpha Vantage API Connection
-alphaV_key = "PROU8P67UYAV1LB4"
-ts = TimeSeries(alphaV_key, output_format='pandas')
-ti = TechIndicators(alphaV_key, output_format='pandas')
+import time
 
 
 
-class Trade:
+class open_position:
+    def __init__(self, symbol, qty, price_filled, term):
+        self.decrease = .01
+
+        self.symbol = symbol
+        self.qty = qty
+        self.price_filled = price_filled
+        self.term = term
+
+        self.limit_loss_price = self.price_filled * (1-self.decrease)
+
+class trade:
     def __init__(self):
-        self.symbol  = input("Enter Symbol:  ")
-        self.qty = input("Enter qty:  ")
-        trade = int(input("1 to buy. 2 to sell. 3 for data :   "))
-        if trade == 1:
-            self.buyOrder()
-        if trade == 2:
-            self.sellOrder()
-        if trade == 3:
-            self.monitor()
-            
+        self.rsi_low = 30
+        self.rsi_high = 70
+        self.freq = 1
+        self.pos = 0
+        
+        self.price = 5
+        self.rsi = 5
 
-            
+        while True:
+            self.data()
+            self.algo()
+            time.sleep(self.freq)
+
+     
+
+    def data(self):
+        # Pings API for upto date data then saves in variables
+        #Test data
+        self.price = self.price + 5
+        self.rsi = self.rsi + 5
+
+    def algo(self):
+        print("monitoring rsi ")
+        if self.rsi <= self.rsi_low:
+            self.lock_price = self.price
+            self.buyOrder()
 
     def monitor(self):
-        data, meta_data = ts.get_intraday(symbol=self.symbol, interval = '1min', outputsize = 'full')
-    
-        all_close_data = data['4. close']
+        while self.pos == 1:
+            self.data()
+            time.sleep(self.freq)
 
-        last_close = all_close_data[0]
-        print(all_close_data)
-        print(last_close)
-        
+            if self.price > self.current_pos.price_filled:
+                print("Current Price: {}".format(self.price))
+                print("Price filled: {}".format(self.current_pos.price_filled))
+
+                if self.rsi > self.rsi_high:
+                    print("Current RSI: {}".format(self.rsi))
+                    self.sellOrder()
+                    
+            elif self.price < self.current_pos.price_filled:
+                if self.price <= self.current_pos.limit_loss_price:
+                    self.sellOrder()
+
     def buyOrder(self):
-        api.submit_order(
+        self.pos = 1
+        print("buy function has been issued")
+        # Will buy the posistion at the lock_price - need to confirm the API before improving this.
+
+        """
+        api.submit_order()
             symbol=self.symbol,
             qty=self.qty,
             side="buy",
             type="market",
             time_in_force="gtc"
         )
+        """
+
+        self.current_pos = open_position("AAPL", 100, self.lock_price, "open")
+        self.monitor()
 
     def sellOrder(self):
+        self.pos = 0
+        print("sell function has been issued")
+        print("Buy price: {}".format(self.current_pos.price_filled))
+        return
+
+        """
         api.submit_order(
             symbol=self.symbol,
             qty=self.qty,
@@ -64,9 +95,10 @@ class Trade:
             type="market",
             time_in_force="gtc"
         )
+        """
 
 if __name__ == "__main__":
-    Trade()
+    trade()
 
 
 
