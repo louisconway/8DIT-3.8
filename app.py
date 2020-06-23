658d0c3c35606c48401b7a44d425545238cef889
louisconway <50680487+louisconway@users.noreply.github.com>
2020-06-24 10:02:45 +1200

    Connected with API for data, now working in the Algo
    Signed-off-by: louisconway <50680487+louisconway@users.noreply.github.com>

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import json
 import datetime
 import pandas as pd 
 import time
-
+from alpha_vantage.timeseries import TimeSeries 
+from alpha_vantage.techindicators import TechIndicators
 
 
 class open_position:
@@ -14,31 +15,51 @@ class open_position:
         self.qty = qty
         self.price_filled = price_filled
         self.term = term
-
         self.limit_loss_price = self.price_filled * (1-self.decrease)
 
 class trade:
     def __init__(self):
         self.rsi_low = 30
         self.rsi_high = 70
-        self.freq = 1
+        self.freq = 60
         self.pos = 0
-        
-        self.price = 5
-        self.rsi = 5
+        self.symbol = "AAPL"
 
+        # Alpaca Markets API Connection
+        API_KEY = "PK6VPNP2RHFM0XSYHB13"
+        SECRET_KEY = "m1kvvD11jhqsFWVYyFp2d9tmvoZumMpIPZKBtCCg"
+        BASE_URL = "https://paper-api.alpaca.markets"
+        api = tradeapi.REST (
+        base_url =BASE_URL,
+        key_id=API_KEY,
+        secret_key=SECRET_KEY
+        )
+
+        # Alpha Vantage API Connection
+        self.alphaV_key = "PROU8P67UYAV1LB4"
+        self.ts = TimeSeries(self.alphaV_key)
+        self.ti = TechIndicators(self.alphaV_key)
+        self.ts = TimeSeries(self.alphaV_key, output_format='pandas')
+        self.ti = TechIndicators(self.alphaV_key, output_format='pandas')
+        
+    
         while True:
             self.data()
             self.algo()
             time.sleep(self.freq)
 
-     
+    def data(self):        
+        # Gets last minute price
+        price_data, meta_data = self.ts.get_intraday(symbol=self.symbol, interval = '1min', outputsize = 'full')
+        price_close_data = price_data['4. close']
+        self.price = price_close_data[0]
+        print("Price of {}: {}".format(self.symbol, self.price))
 
-    def data(self):
-        # Pings API for upto date data then saves in variables
-        #Test data
-        self.price = self.price + 5
-        self.rsi = self.rsi + 5
+        # Gets last RSI minute price
+        currentRSI_data, meta_data = self.ti.get_rsi(symbol=self.symbol, interval='1min', time_period=14, series_type='close')
+        rsi_close = currentRSI_data['RSI']
+        self.rsi = rsi_close[0]
+        print("current RSI: {}".format(self.rsi))
 
     def algo(self):
         print("monitoring rsi ")
@@ -66,17 +87,15 @@ class trade:
     def buyOrder(self):
         self.pos = 1
         print("buy function has been issued")
-        # Will buy the posistion at the lock_price - need to confirm the API before improving this.
 
-        """
         api.submit_order()
             symbol=self.symbol,
-            qty=self.qty,
+            qty=1,
             side="buy",
             type="market",
             time_in_force="gtc"
         )
-        """
+     
 
         self.current_pos = open_position("AAPL", 100, self.lock_price, "open")
         self.monitor()
