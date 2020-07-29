12058313d8d9a09af784a8d49b55f4c27ab0f14c
louisconway <50680487+louisconway@users.noreply.github.com>
2020-07-29 12:44:04 +1200

    rendering using html template
    Signed-off-by: louisconway <50680487+louisconway@users.noreply.github.com>

--- websitescript.py
+++ websitescript.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello this is the main page <h1>HELLO<h1>"
+    return render_template("index.html")
 
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 if __name__ == "__main__":
     app.run()
