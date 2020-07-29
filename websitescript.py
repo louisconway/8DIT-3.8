bdb45dd1ba4ad7e84dd6347370fc4401be36b2ca
louisconway <50680487+louisconway@users.noreply.github.com>
2020-07-29 13:31:19 +1200

    python script running in html
    Signed-off-by: louisconway <50680487+louisconway@users.noreply.github.com>

--- websitescript.py
+++ websitescript.py
@@ -2,12 +2,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=["tim", "jo", "bill"])
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
 if __name__ == "__main__":
     app.run()
