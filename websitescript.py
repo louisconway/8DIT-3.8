d11200a322beb264fa20118a82f5802a5d88a855
louisconway <50680487+louisconway@users.noreply.github.com>
2020-08-04 09:34:37 +1200

    navigation working
    Signed-off-by: louisconway <50680487+louisconway@users.noreply.github.com>

--- websitescript.py
+++ websitescript.py
@@ -1,10 +1,15 @@
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
+balance = 100
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=["tim", "jo", "bill"])
+@app.route("/")
+def home():
+    return render_template("index.html", content=10)
+
+@app.route("/trade")
+def trade():
+    return render_template("trade.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
