2a0c129768e5328ae29fbf04da0335ebc30ba2ce
louisconway <50680487+louisconway@users.noreply.github.com>
2020-11-02 08:46:06 +1300

    final
    Signed-off-by: louisconway <50680487+louisconway@users.noreply.github.com>

--- app.py
+++ app.py
@@ -29,6 +29,8 @@ class trade:
         self.equity_management()
         time.sleep(3)
         self.buy()
+        time.sleep(4)
+        self.close_pos()
 
     def login(self):
         # Working Well
@@ -38,13 +40,14 @@ class trade:
 
         while True:
             try:
-                email_type = driver.find_element_by_xpath('/html/body/div[25]/div/div[2]/div[2]/div[1]/div/input')
+                email_type = driver.find_element_by_xpath('/html/body/div[26]/div/div[2]/div[2]/div[1]/div/input')
                 email_type.send_keys('louis.conway123@gmail.com')
-                password_type = driver.find_element_by_xpath('/html/body/div[25]/div/div[2]/div[2]/div[2]/div/input')
-                password_type.send_keys('Ld5gh3rr')
+                password_type = driver.find_element_by_xpath('/html/body/div[26]/div/div[2]/div[2]/div[2]/div/input')
+                password_type.send_keys('**')
                 confirm_click = driver.find_element_by_xpath('//*[@id="l_overlay"]/div/div[2]/button')
                 confirm_click.click()
                 print("Logged In")
+                print('end')
                 break
             except:
                 print("trying...")
@@ -66,20 +69,22 @@ class trade:
         temp_equity = temp.replace(",","")
         self.equity = float(temp_equity)
         print(self.equity)
-
     
     def search(self):
         # Searching the correct ticker
         print("search has started")
         while True:
             try:
-                time.sleep(10)
+                time.sleep(3)
                 search_company = driver.find_element_by_xpath('/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/div/div[1]/trade-topbar/div/div[1]/trade-search-field/div/input')
                 search_company.send_keys(self.company_name)
-                time.sleep(5)
+                time.sleep(3)
                 break
             except:
                 print("failed")
+        
+        click_comp = driver.find_element_by_xpath('/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/div/div/div/trade-instruments-list/scroll-pane/div[1]/trade-instruments-button[1]/div/div[1]')
+        click_comp.click()
         
         get_ticker = driver.find_element_by_xpath('/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/trade-side-panel/div/div/div[3]/trade-market-info/div/scroll-pane/div[1]/div[2]/div[1]/div').text
         print(get_ticker)
@@ -90,7 +95,7 @@ class trade:
             trade_button_click = driver.find_element_by_xpath('/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/trade-side-panel/div/div/div[2]/div/div[1]')
             trade_button_click.click()
         else:
-            print("Moving to next stock in list, unable to locate the correct stock")
+            print("Unable to locate the correct stock")
 
     def get_price(self):
         while True:
@@ -107,12 +112,9 @@ class trade:
         self.share_num = self.trade_amount / self.current_price
         self.stop_loss = self.current_price * self.stop_loss_per
         self.take_profit = self.current_price * self.take_prof_per
-
         
-        #self.loss_forcast = self.stop_loss * self.share_num
-        self.loss_forcast = 50.00
+        self.loss_forcast = self.stop_loss * self.share_num
         self.profit_forecast = self.take_profit * self.share_num
-
 
         print("Trade Equity: {}".format(self.trade_amount))
         print("Volume of Shares: {:.2f}".format(self.share_num))
@@ -120,6 +122,7 @@ class trade:
         print("Take Profit: {}".format(self.take_profit))
 
     def buy(self):
+        # need to confirm if this is actually buy button.
         buy_selection_click = driver.find_element_by_xpath('/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/div/div[1]/div/trade-instruments-list/scroll-pane/div[1]/trade-instruments-button[1]/div/div[4]/div[2]')
         buy_selection_click.click()
 
@@ -128,12 +131,24 @@ class trade:
                 #Summit Trade
                 buy_button_click = driver.find_element_by_xpath('/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/trade-side-panel/div/div/div[3]/trade-deal-ticket/place-order/div/div[1]/div')
                 buy_button_click.click() 
-                print("done :)")
+                print("position has been filled")
                 break      
             except:
-                print("attempting to click the button")
-    def sell(self):
-        pass
+                print("attempting to click the buy button")
+    
+    def close_pos(self):
+        pos_tab = driver.find_element_by_xpath('/html/body/app-root/div/left-side-panel/div[2]/div[1]/popup-button[1]')
+        pos_tab.click()
+        while True:
+            p_l_num = driver.find_element_by_xpath('/html/body/app-root/div/left-side-panel/div[1]/div[2]/positions-popup/positions-list2/scroll-pane/div[1]/positions-list-item2[2]/positions-list-sub-item2/div[9]/span').text
+            print(p_l_num)
+            if p_l_num >= self.take_profit:
+                close_pos = driver.find_element_by_xpath('/html/body/app-root/div/left-side-panel/div[1]/div[2]/positions-popup/positions-list2/scroll-pane/div[1]/positions-list-item2[2]/positions-list-sub-item2/div[10]/close-position-button/div')
+                close_pos.click()
+                break
+            else:
+                time.sleep(3)
+        
 
 class screener:
     def __init__(self, url):
@@ -179,8 +194,8 @@ class screener:
             except:
                 print("loading")
 
-#website = screener('https://finviz.com')
-#print(website.ticker)
-#print(website.company_name)
+website = screener('https://finviz.com')
+print(website.ticker)
+print(website.company_name)
 
-trade_site = trade('https://capital.com', "T", "AT&T Inc")
+trade_site = trade('https://capital.com', website.ticker, website.company_name)
